0532df9a085226dbbcb31b03d72e6189d73116c6
Terrence <owusuterrence@gmail.com>
2018-09-12 03:30:39 +0100

    added example questioin for queues

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,21 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+	// Check whether there are items in any Queue
+
+	const q = new Queue();
+
+	while (sourceOne.peek() || sourceTwo.peek()) {
+		if (sourceOne.peek()) {
+			q.add(sourceOne.remove());
+		}
+
+		if (sourceTwo.peek()) {
+			q.add(sourceTwo.remove());
+		}
+	}
+	return q;
+}
 
 module.exports = weave;
